391d14b8fff431d97ab4ddd7857f11302d0d9ae3
Henry <henrysungyun@gmail.com>
2023-02-05 08:36:59 -0800

    come back and finish this

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  console.log("The DOM has loaded");
+});
+
+const button = document.querySelector('#create-task-form');
+button.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const taskDescription = e.target['new-task-description'].value;
+  const newTask = document.createElement('li');
+  newTask.textContent = taskDescription;
+  document.getElementById('tasks').append(newTask);
 });
